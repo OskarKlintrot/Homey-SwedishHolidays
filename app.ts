c22584977fc814383247dcefc07e787bb514cade
Oskar Klintrot <oskar.klintrot@gmail.com>
2026-04-28 22:18:08 +0200

    revert

--- app.ts
+++ app.ts
@@ -37,11 +37,11 @@ const TOKEN_IDS = {
 } as const;
 
 const TOKEN_TITLES = {
-  holidayName: 'Svensk helgdag',
-  isWorkdayBridgeDaysOff: 'Svensk arbetsdag',
-  isWorkdayBridgeDaysOn: 'Svensk arbetsdag eller klämdag',
-  isBridgeDay: 'Svensk klämdag',
-  isPublicHoliday: 'Svensk röd dag',
+  holidayName: 'Swedish holiday name',
+  isWorkdayBridgeDaysOff: 'Swedish workday',
+  isWorkdayBridgeDaysOn: 'Swedish workday or bridge day',
+  isBridgeDay: 'Swedish bridge day',
+  isPublicHoliday: 'Swedish public holiday',
 } as const;
 
 const FALLBACK_TIME_ZONE = 'Europe/Stockholm';
